cebc5278049ec3341f4afa7e21c43d06abf314e3
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2024-02-16 23:33:25 -0700

    Successful displayed data on the web page from a database

---
+++ movies/src/main/java/dev/viktorbabych/movies/MovieService.java
@@ -0,0 +1,18 @@
+package dev.viktorbabych.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return  movieRepository.findAll();
+
+    }
+}
