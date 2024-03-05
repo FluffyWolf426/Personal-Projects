6dc3f3f51fcdb939d2be871d27621251d33bba55
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2024-03-04 18:24:38 -0700

    update http

--- MovieApp/MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieApp/MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'https://aa7e-2604-3d09-867b-860-dcc6-29c8-6dc7-5ae6.ngrok-free.app',
+    baseURL:'https://fdf5-192-197-128-21.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
